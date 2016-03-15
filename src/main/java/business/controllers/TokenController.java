ea728a8b42ca71fc8ccd8d1954e592f6509658c1
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2016-03-15 17:43:22 +0100

    Se eliminan comentarios

--- src/main/java/business/controllers/TokenController.java
+++ src/main/java/business/controllers/TokenController.java
@@ -27,20 +27,12 @@ public class TokenController {
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
+    
+    
 
     public String login(String username) {
         User user = userDao.findByUsernameOrEmail(username);
         assert user != null;
-        //TODO:Comprobar si el usuario logeado tiene un token valido
-    
-        //String last_token = tokenDao.findLastToken(user)
-        //String seconds24hours = XXXXXX
-        //if(last_token >  seconds24hours)
-        //  renovar token
-        //else
-        // recoger token y enviar
-        
-        
         Token token = new Token(user);
         tokenDao.save(token);
         return token.getValue();
