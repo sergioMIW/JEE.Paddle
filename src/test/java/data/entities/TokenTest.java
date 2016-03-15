ca37609e0f077b95b2ff3533c7259327a6223dca
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2016-03-15 20:15:50 +0100

    Test para comprobar la validacion del token

--- src/test/java/data/entities/TokenTest.java
+++ src/test/java/data/entities/TokenTest.java
@@ -1,13 +1,12 @@
 package data.entities;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import org.junit.Test;
-
-import data.entities.Token;
-import data.entities.User;
 
 public class TokenTest {
 
@@ -16,6 +15,26 @@ public class TokenTest {
         User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
         Token token = new Token(user);
         assertTrue(token.getValue().length() > 20);
+    }
+
+    @Test
+    public void testTokenIsExpired() {
+        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+        Token token = new Token(user);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date()); // sets calendar time/date
+        calendar.add(Calendar.HOUR_OF_DAY, -2); // less two hour
+        token.setCreateConnection(calendar);
+        assertTrue(token.isValidTime());
+    }
+
+    @Test
+    public void testTokenIsNotExpired() {
+        User user = new User("u", "u@gmail.com", "p", Calendar.getInstance());
+        Token token = new Token(user);
+        Calendar calendar = Calendar.getInstance();
+        token.setCreateConnection(calendar);
+        assertFalse(token.isValidTime());
     }
 
 }
