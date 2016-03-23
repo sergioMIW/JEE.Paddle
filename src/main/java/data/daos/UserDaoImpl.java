121607ef2c2147095782a0de9cda7a16ac94d927
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2016-03-23 20:09:44 +0100

    Injeccion de Daos

--- src/main/java/data/daos/UserDaoImpl.java
+++ src/main/java/data/daos/UserDaoImpl.java
@@ -1,12 +1,16 @@
 package data.daos;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import data.entities.Token;
 import data.entities.User;
 
 public class UserDaoImpl implements UserDaoExtended {
-
+    
+    @Autowired
     private TokenDao tokenDao;
 
+    @Autowired
     private UserDao userDao;
 
     public User findByTokenValueIsValid(String usernameOrEmailOrTokenValue) {
