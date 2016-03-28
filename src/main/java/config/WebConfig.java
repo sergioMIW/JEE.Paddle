26a3e3d1794b37c0d18b95686efd057718bf6388
Sergio Gil <sergio.giljimenez@alumnos.upm.es>
2016-03-28 22:15:44 +0200

    FUSION

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -13,7 +13,8 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {ResourceNames.REST_API, ResourceNames.CONTROLLERS, ResourceNames.DAOS, ResourceNames.SERVICES , ResourceNames.WEB})
+@ComponentScan(basePackages = {ResourceNames.REST_API, ResourceNames.CONTROLLERS, ResourceNames.DAOS, ResourceNames.SERVICES,
+        ResourceNames.WEB})
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     // CORS
@@ -21,7 +22,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*").maxAge(3600);
     }
-    
+
     @Bean
     public ViewResolver jspViewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -33,7 +34,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         viewResolver.setViewNames("jsp/*", "bootstrap/*");
         return viewResolver;
     }
-    
+
     // Web presenters
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
